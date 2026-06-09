44148c7dbb05e765678f6a3719c20b0b2d44bfe2
iamkaifyyy <mkaifm728@gmail.com>
2026-06-09 19:58:56 +0530

    Resumed Logs Updated

--- week-15/brainly/src/index.ts
+++ week-15/brainly/src/index.ts
@@ -1,11 +1,23 @@
 import express from "express";
 import mongoose from "mongoose";
 import jwt from "jsonwebtoken"
+import { userModel } from "./db.js";
 
 const app = express();
 
-app.post("/api/v1/signup", (req, res) => {
+app.post("/api/v1/signup", async (req, res) => {
+    // zod validation here, hash the password 
+    const user = req.body.username;
+    const password = req.body.password;
 
+    await userModel.create({
+        username: user,
+        password: password
+    })
+
+    res.json({
+        message: "User Signed up"
+    })
 });
 
 app.post("/api/v1/signin", (req, res) => {
