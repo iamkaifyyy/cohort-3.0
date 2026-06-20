2a05ced1c5b25fb9943dd4cceb0d910055c5fcc5
iamkaifyyy <mkaifm728@gmail.com>
2026-06-20 12:19:51 +0530

    LogsAdded

---
+++ week-18-next/app/signin/page.tsx
@@ -0,0 +1,13 @@
+export default function Signin(){
+    return <div>
+
+        Signin Page
+        <input type="text" placeholder="email" />
+        <input type="password" placeholder="password"/>
+
+
+        <button>
+            Sign in 
+        </button>
+    </div>
+}
---
+++ week-18-next/app/signup/page.tsx
@@ -0,0 +1,13 @@
+export default function Signup(){
+    return <div>
+
+        Signup Page
+        <input type="text" placeholder="email" />
+        <input type="password" placeholder="password"/>
+
+
+        <button>
+            Sign up 
+        </button>
+    </div>
+}
